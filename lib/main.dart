71aea9b5dd764b42c07359b7cc7dbff81f4deada
gamenic-pawanrai <gamenic.pawanrai@gmail.com>
2022-01-20 14:27:41 +0545

    4.15 String interpolation

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class MyHomePage extends StatelessWidget {
                         ),
                       ),
                       child: Text(
-                        tx.amount.toString(),
+                        '\$${tx.amount}',
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
