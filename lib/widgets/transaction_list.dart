8e2e5fc2fb2b85ea5dd9e47fc50dac2e03cc33f1
gamenic-pawanrai <gamenic.pawanrai@gmail.com>
2022-01-24 18:40:09 +0545

    4.24V2 ListBuilder()

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,8 +10,8 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView(
-        children: transactions.map((tx) {
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
           return Card(
             child: Row(
               children: <Widget>[
@@ -28,7 +28,7 @@ class TransactionList extends StatelessWidget {
                     ),
                   ),
                   child: Text(
-                    '\$${tx.amount}',
+                    '\$${transactions[index].amount}',
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
@@ -39,14 +39,16 @@ class TransactionList extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     Text(
-                      tx.title,
+                      transactions[index].title,
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 16,
                       ),
                     ),
                     Text(
-                      DateFormat().add_yMMMMd().format(tx.date),
+                      DateFormat()
+                          .add_yMMMMd()
+                          .format(transactions[index].date),
                       style: TextStyle(
                         fontSize: 16,
                         color: Colors.grey[800],
@@ -57,8 +59,58 @@ class TransactionList extends StatelessWidget {
               ],
             ),
           );
-        }).toList(),
+        },
+        itemCount: transactions.length,
       ),
+      // child: ListView(
+      //   children: transactions.map((tx) {
+      //     return Card(
+      //       child: Row(
+      //         children: <Widget>[
+      //           Container(
+      //             margin: EdgeInsets.symmetric(
+      //               vertical: 10,
+      //               horizontal: 15,
+      //             ),
+      //             padding: EdgeInsets.all(10),
+      //             decoration: BoxDecoration(
+      //               border: Border.all(
+      //                 color: Colors.purple,
+      //                 width: 2,
+      //               ),
+      //             ),
+      //             child: Text(
+      //               '\$${tx.amount}',
+      //               style: TextStyle(
+      //                   fontWeight: FontWeight.bold,
+      //                   fontSize: 20,
+      //                   color: Colors.purple),
+      //             ),
+      //           ),
+      //           Column(
+      //             crossAxisAlignment: CrossAxisAlignment.start,
+      //             children: <Widget>[
+      //               Text(
+      //                 tx.title,
+      //                 style: TextStyle(
+      //                   fontWeight: FontWeight.bold,
+      //                   fontSize: 16,
+      //                 ),
+      //               ),
+      //               Text(
+      //                 DateFormat().add_yMMMMd().format(tx.date),
+      //                 style: TextStyle(
+      //                   fontSize: 16,
+      //                   color: Colors.grey[800],
+      //                 ),
+      //               ),
+      //             ],
+      //           )
+      //         ],
+      //       ),
+      //     );
+      //   }).toList(),
+      // ),
     );
   }
 }
