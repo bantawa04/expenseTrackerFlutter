76b3392892f894ef8bd57eb7232ce402ca9b6a56
bantawa04 <rahunn3@gmail.com>
2022-05-15 19:55:21 +0545

    4.34 Planning chart

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  const Chart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Container(
+        child: Row(
+          children: <Widget>[
+            Column()
+          ],
+        ),
+      ),
+    );
+  }
+}
