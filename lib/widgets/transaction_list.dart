00a0e68ad6f51a07a4fc7256e8910f36c27a67ef
bantawa04 <rahunn3@gmail.com>
2022-05-30 21:02:02 +0545

    module 4 complete wrap-up

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,7 +11,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 450,
       child: transactions.isEmpty
           ? Column(
               children: <Widget>[
