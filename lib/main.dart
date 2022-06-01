e9d98d28796e08d1399f884aa373786edac7887a
bantawa04 <rahunn3@gmail.com>
2022-06-01 14:28:31 +0545

    5.6 Using layout builder

--- lib/main.dart
+++ lib/main.dart
@@ -161,7 +161,7 @@ class _MyHomePageState extends State<MyHomePage> {
               height: (MediaQuery.of(context).size.height -
                       appBar.preferredSize.height -
                       MediaQuery.of(context).padding.top) *
-                  0.4,
+                  0.3,
               child: Chart(_recentTransactions),
             ),
             Container(
@@ -169,7 +169,7 @@ class _MyHomePageState extends State<MyHomePage> {
               height: (MediaQuery.of(context).size.height -
                       appBar.preferredSize.height -
                       MediaQuery.of(context).padding.top) *
-                  0.6,
+                  0.7,
               child: TransactionList(_userTransaction, _deleteTransation),
             )
           ],
--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -11,57 +11,64 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          height: 20,
-          child: FittedBox(
-            //forces its child into the available space
-            child: Text('\$${spendingAmount.toString()}'),
+    return LayoutBuilder(builder: (ctx, constraint) {
+      return Column(
+        children: <Widget>[
+          Container(
+            height: constraint.maxHeight * 0.15,
+            child: FittedBox(
+              //forces its child into the available space
+              child: Text('\$${spendingAmount.toString()}'),
+            ),
           ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 1),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              FractionallySizedBox(
-                heightFactor: spendingPercentage,
-                child: Container(
+          SizedBox(
+            height: constraint.maxHeight * 0.05,
+          ),
+          Container(
+            height: constraint.maxHeight * 0.6,
+            width: 10,
+            child: Stack(
+              children: <Widget>[
+                Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
+                    border: Border.all(color: Colors.grey, width: 1),
+                    color: Color.fromRGBO(220, 220, 220, 1),
                     borderRadius: BorderRadius.circular(20),
                   ),
                 ),
-              ),
-            ],
-          ), //overlap widgets
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text(
-          label,
-          style: today
-              ? TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontWeight: FontWeight.bold,
-                )
-              : TextStyle(
-                  fontWeight: FontWeight.bold,
+                FractionallySizedBox(
+                  heightFactor: spendingPercentage,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
                 ),
-        ),
-      ],
-    );
+              ],
+            ), //overlap widgets
+          ),
+          SizedBox(
+            height: constraint.maxHeight * 0.05,
+          ),
+          Container(
+            height: constraint.maxHeight * 0.15,
+            child: FittedBox(
+              child: Text(
+                label,
+                style: today
+                    ? TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.bold,
+                      )
+                    : TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+              ),
+            ),
+          ),
+        ],
+      );
+    });
   }
 }
