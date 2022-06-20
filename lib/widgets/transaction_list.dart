322a9053e59ae86fb663522ba9d4d95c43a381e5
bantawa04 <rahunn3@gmail.com>
2022-06-20 15:31:10 +0545

    5.13 show hide button text on device width

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -58,13 +58,23 @@ class TransactionList extends StatelessWidget {
                   subtitle: Text(
                     DateFormat.yMMMd().format(transactions[index].date!),
                   ),
-                  trailing: IconButton(
-                    onPressed: (() => deleteTx(transactions[index].id)),
-                    icon: Icon(
-                      Icons.delete,
-                      color: Theme.of(context).errorColor,
-                    ),
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 460
+                      ? TextButton.icon(
+                          onPressed: (() => deleteTx(transactions[index].id)),
+                          icon: Icon(Icons.delete,
+                              color: Theme.of(context).errorColor),
+                          label: Text(
+                            'Delete',
+                            style:
+                                TextStyle(color: Theme.of(context).errorColor),
+                          ))
+                      : IconButton(
+                          onPressed: (() => deleteTx(transactions[index].id)),
+                          icon: Icon(
+                            Icons.delete,
+                            color: Theme.of(context).errorColor,
+                          ),
+                        ),
                 ),
               );
             },
